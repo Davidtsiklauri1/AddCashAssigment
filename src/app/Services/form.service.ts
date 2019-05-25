4a8598840968dbee6484c18f3855c4d6d44f8cb0
David <wiklauri.dato1997@gmail.com>
2019-05-25 19:56:49 +0300

    ///\\\

---
+++ src/app/Services/form.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class FormService {
+  constructor() {}
+arr = [];
+  get(value:string) {
+    let str = value,arrIndex = [0],arrs = [],arrNumbers = [],val;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "," || str[i] === "\n" || str[i] === ";") {
+        arrIndex.push(i + 1);
+      }
+    }
+
+    for (let i = 0; i < arrIndex.length; i++) {
+      let s = str.slice(arrIndex[i], arrIndex[i + 1]);
+      arrs.push(s);
+    }
+    for (let i = 0; i < arrs.length; i++) {
+      if (arrs[i].match(/[a-z]|[A-Z]/g)) {
+        console.log("Do Nothing");
+      } else if (arrs[i].indexOf(",") >= 0) {
+        let item = arrs[i].replace(/,/g, "");
+        arrNumbers.push(item);
+      } else if (arrs[i].indexOf(";") >= 0) {
+        let item = arrs[i].replace(/;/g, "");
+        arrNumbers.push(item);
+      } else if (arrs[i].indexOf("\n") >= 0) {
+        let item = arrs[i].replace(/\n/g, "");
+        arrNumbers.push(item);
+      } else {
+        arrNumbers.push(arrs[i]);
+      }
+    }
+    val = arrNumbers.map(val => Number(val));
+   this.arr.push(...val);
+this.saveLocalStorage(this.arr);
+
+
+
+
+
+}
+saveLocalStorage(arr){
+
+
+localStorage.setItem("key", JSON.stringify(arr));
+this.LoadLocalStorage();
+}
+
+LoadLocalStorage(){
+let item = JSON.parse(localStorage.getItem("key"));
+return item;
+
+}
+delete(arr,i){
+let elem = JSON.parse(localStorage.getItem("key"));
+elem.splice(i,1);
+arr.splice(i,1);
+localStorage.setItem('key',JSON.stringify(arr));
+
+
+}
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { FormManagmentComponent } from './form-managment/form-managment.component';
+import {FormService} from './Services/form.service';
+import { ItemListComponent } from './item-list/item-list.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormManagmentComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
